d2e020d99f36c10ed04bfa2693aaead945c8fb21
Milton Fernandez <miltonfernandez17@yahoo.com>
2021-02-22 21:12:22 -0600

    started setup on Flashcards Page

--- src/components/Flashcards/Flashcards.jsx
+++ src/components/Flashcards/Flashcards.jsx
@@ -1,6 +1,36 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+import './Flashcards.css';
 function Flashcards(){
+    useEffect(() => {
+        dispatch({ type: 'FETCH_DATA' });
+    }, []);
+    const dispatch = useDispatch();
+    const dataStore = useSelector(store => store.data);
+    const [flashcard, setNewFlashcard] = useState([]);
+ 
+    //function to pick a quiz by its title
+    function getByTitle(title_name) {
+        let title = title_name;
+        console.log(title);
+        let newFlashcard = [];
+        for (let i = 0; i < dataStore.length; i++) {
+            if (dataStore[i].title == title) {
+                newFlashcard.push(dataStore[i]);
+            }
+        }
+        setNewFlashcard(newFlashcard);
+    
+        return;
+    }
+console.log(flashcard);
     return(
         <>
+        <div classname="center">
+        <button value = "hiragana" onClick={(e) => getByTitle(e.target.value)}>Hiragana</button>
+        <button value = "katakana" onClick={(e) => getByTitle(e.target.value)}>Katakana</button>
+        </div>
         </>
     )
 }
