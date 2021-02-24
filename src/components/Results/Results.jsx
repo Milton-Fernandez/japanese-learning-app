b3bebed660f7a6cff840ebf7bcc57b3606c8a03e
Milton Fernandez <miltonfernandez17@yahoo.com>
2021-02-24 00:36:33 -0600

    made result page functional

--- src/components/Results/Results.jsx
+++ src/components/Results/Results.jsx
@@ -1,6 +1,36 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useState } from 'react';
+
 function Results(){
+    const dispatch = useDispatch();
+   
+    const dataStore = useSelector(store => store.result);
+    const user = useSelector((store) => store.user);
+    useEffect(() => {
+        dispatch({ type: 'FETCH_RESULT' });
+    }, []);
+    console.log(dataStore);
     return(
         <>
+            <h2>Results For: {user.username}!</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Correct</th>
+                        <th>Incorrect</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {dataStore.map((data) =>
+                        <tr>
+                            <td>{data.correct}</td>
+                            <td>{data.incorrect}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+
         </>
     )
 }
